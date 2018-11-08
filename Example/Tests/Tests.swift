4074ac9e9217781d0c58321ba9675c035de62edf
Antoine van der Lee <antoine@wetransfer.com>
2018-11-08 18:01:15 +0100

    Fixed CI warnings and errors, excluded pods from linting

--- Example/Tests/Tests.swift
+++ Example/Tests/Tests.swift
@@ -18,11 +18,4 @@ class Tests: XCTestCase {
         XCTAssert(true, "Pass")
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure() {
-            // Put the code you want to measure the time of here.
-        }
-    }
-    
 }
