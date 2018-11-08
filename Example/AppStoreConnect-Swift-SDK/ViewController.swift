77d73561ef1f6b4bd666c50e8a34216dd4a16d32
Antoine van der Lee <antoine@wetransfer.com>
2018-11-08 17:21:05 +0100

    Tests are working again, added an example application

---
+++ Example/AppStoreConnect-Swift-SDK/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AppStoreConnect-Swift-SDK
+//
+//  Created by ajvanderlee@gmail.com on 11/08/2018.
+//  Copyright (c) 2018 ajvanderlee@gmail.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
---
+++ Example/Pods/Target Support Files/Pods-AppStoreConnect-Swift-SDK_Example/Pods-AppStoreConnect-Swift-SDK_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AppStoreConnect_Swift_SDK_Example : NSObject
+@end
+@implementation PodsDummy_Pods_AppStoreConnect_Swift_SDK_Example
+@end
