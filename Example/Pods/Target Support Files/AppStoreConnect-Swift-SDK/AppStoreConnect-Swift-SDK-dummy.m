d2b7d22fa362b850ed3218bcc029d6622858de2f
Antoine van der Lee <antoine@wetransfer.com>
2018-11-10 10:18:25 +0100

    Remove pods folder from git

--- Example/Pods/Target Support Files/AppStoreConnect-Swift-SDK/AppStoreConnect-Swift-SDK-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_AppStoreConnect_Swift_SDK : NSObject
-@end
-@implementation PodsDummy_AppStoreConnect_Swift_SDK
-@end
--- Example/Pods/Target Support Files/Pods-AppStoreConnect-Swift-SDK_Example/Pods-AppStoreConnect-Swift-SDK_Example-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_AppStoreConnect_Swift_SDK_Example : NSObject
-@end
-@implementation PodsDummy_Pods_AppStoreConnect_Swift_SDK_Example
-@end
