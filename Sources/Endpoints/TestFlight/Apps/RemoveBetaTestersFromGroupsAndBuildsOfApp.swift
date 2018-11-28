f9a6b91fc98bc0dc3a7df03fd94f21772fba48a8
Pascal Edmond <pe@pierlis.com>
2018-11-28 13:19:45 +0100

    RemoveBetaTestersFromGroupsAndBuildsOfApp knows how to make its body

--- Sources/Endpoints/TestFlight/Apps/RemoveBetaTestersFromGroupsAndBuildsOfApp.swift
+++ Sources/Endpoints/TestFlight/Apps/RemoveBetaTestersFromGroupsAndBuildsOfApp.swift
@@ -4,10 +4,11 @@
 //
 //  Created by Pascal Edmond on 26/11/2018.
 //
-    
+
+import Foundation
 import Alamofire
 
-extension APIEndpoint {
+extension APIEndpoint where T == Void {
 
     /// Remove one or more beta testers' access to test any builds of a specific app.
     ///
@@ -15,10 +16,11 @@ extension APIEndpoint {
     ///   - id: (Required) An opaque resource ID that uniquely identifies the resource.
     public static func remove(
         betaTesters: AppBetaTestersLinkagesRequest,
-        fromGroupsAndBuildsOfAppWithId id: String) -> APIEndpoint<Void> {
-        return APIEndpoint<Void>(
+        fromGroupsAndBuildsOfAppWithId id: String) -> APIEndpoint {
+        return APIEndpoint(
             path: "apps/\(id)/relationships/betaTesters",
             method: .delete,
-            parameters: ["AppBetaTestersLinkagesRequest": betaTesters])
+            parameters: nil,
+            body: try? JSONEncoder().encode(betaTesters))
     }
 }
