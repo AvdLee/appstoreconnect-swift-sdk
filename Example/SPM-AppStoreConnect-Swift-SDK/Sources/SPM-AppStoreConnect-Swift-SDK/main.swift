e8bf88cedadace761ec18c30cc994fa22680045d
Pascal Edmond <pe@pierlis.com>
2018-11-28 15:53:39 +0100

    update SPM example

--- Example/SPM-AppStoreConnect-Swift-SDK/Sources/SPM-AppStoreConnect-Swift-SDK/main.swift
+++ Example/SPM-AppStoreConnect-Swift-SDK/Sources/SPM-AppStoreConnect-Swift-SDK/main.swift
@@ -3,8 +3,7 @@ import AppStoreConnect_Swift_SDK
 
 /// Go to https://appstoreconnect.apple.com/access/api and create your own key. This is also the page to find the private key ID and the issuer ID.
 /// Download the private key and open it in a text editor. Remove the enters and copy the contents over to the private key parameter.
-private let configuration = APIConfiguration(issuerID: "", privateKeyID: "", privateKey: ""
-)
+private let configuration = APIConfiguration(issuerID: "<YOUR ISSUER ID>", privateKeyID: "<YOUR PRIVATE KEY ID>", privateKey: "<YOUR PRIVATE KEY>")
 var provider: APIProvider = APIProvider(configuration: configuration)
 
 let endpoint = APIEndpoint.apps(
