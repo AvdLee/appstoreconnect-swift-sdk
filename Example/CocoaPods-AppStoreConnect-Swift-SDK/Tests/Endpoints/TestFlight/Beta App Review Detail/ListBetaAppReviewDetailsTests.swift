c2af645e92d143ce39a88a0dea8d048a6fa3f612
Pascal Edmond <pe@pierlis.com>
2018-11-30 15:30:26 +0100

    Add TestFlight/Beta App Review Detail endpoints

---
+++ Example/CocoaPods-AppStoreConnect-Swift-SDK/Tests/Endpoints/TestFlight/Beta App Review Detail/ListBetaAppReviewDetailsTests.swift
@@ -0,0 +1,27 @@
+//
+//  ListBetaAppReviewDetailsTests.swift
+//  AppStoreConnect-Swift-SDK_Tests
+//
+//  Created by Pascal Edmond on 30/11/2018.
+//
+
+import XCTest
+@testable import AppStoreConnect_Swift_SDK
+
+final class ListBetaAppReviewDetailsTests: XCTestCase {
+    
+    func testURLRequest() {
+        let endpoint = APIEndpoint.betaAppReviewDetails(
+            fields: [.apps(ListBetaAppReviewDetails.Field.App.allCases),
+                     .betaAppReviewDetails(ListBetaAppReviewDetails.Field.BetaAppReviewDetail.allCases)],
+            filter: [.app(["app"])],
+            include: ListBetaAppReviewDetails.Include.allCases,
+            limit: 2)
+        let request = try? endpoint.asURLRequest()
+        XCTAssertEqual(request?.httpMethod, "GET")
+        
+        let absoluteString = request?.url?.absoluteString
+        let expected = "https://api.appstoreconnect.apple.com/v1/betaAppReviewDetails?fields%5Bapp%5D=betaAppLocalizations%2CbetaAppReviewDetail%2CbetaGroups%2CbetaLicenseAgreement%2CbetaTesters%2Cbuilds%2CbundleId%2Cname%2CpreReleaseVersions%2CprimaryLocale%2Csku&fields%5BbetaAppReviewDetail%5D=app%2CcontactEmail%2CcontactFirstName%2CcontactLastName%2CcontactPhone%2CdemoAccountName%2CdemoAccountPassword%2CdemoAccountRequired%2Cnotes&filter%5Bapp%5D=app&include=app&limit=2"
+        XCTAssertEqual(absoluteString, expected)
+    }
+}
---
+++ Sources/Endpoints/TestFlight/Beta App Review Detail/ListBetaAppReviewDetails.swift
@@ -0,0 +1,88 @@
+//
+//  ListBetaAppReviewDetails.swift
+//  AppStoreConnect-Swift-SDK
+//
+//  Created by Pascal Edmond on 30/11/2018.
+//
+    
+import Alamofire
+
+extension APIEndpoint where T == BetaAppReviewDetailsResponse {
+
+    /// Find and list beta app review details for all apps.
+    ///
+    /// - Parameters:
+    ///   - fields: Fields to return for included related types.
+    ///   - filter: Attributes, relationships, and IDs by which to filter.
+    ///   - include: Relationship data to include in the response.
+    ///   - limit: Number of resources to return.
+    public static func betaAppReviewDetails(
+        fields: [ListBetaAppReviewDetails.Field]? = nil,
+        filter: [ListBetaAppReviewDetails.Filter]? = nil,
+        include: [ListBetaAppReviewDetails.Include]? = nil,
+        limit: Int? = nil) -> APIEndpoint {
+        var parameters = [String: Any]()
+        if let fields = fields { parameters.add(fields) }
+        if let filter = filter { parameters.add(filter) }
+        if let include = include { parameters.add(include) }
+        if let limit = limit { parameters["limit"] = limit }
+        return APIEndpoint(path: "betaAppReviewDetails", method: .get, parameters: parameters)
+    }
+}
+
+public struct ListBetaAppReviewDetails {
+
+    /// Fields to return for included related types.
+    public enum Field: NestableQueryParameter {
+        case apps([App])
+        case betaAppReviewDetails([BetaAppReviewDetail])
+
+        static var key: String = "fields"
+        var pair: Pair {
+            switch self {
+            case .apps(let value):
+                return (App.key, value.map({ $0.pair.value }).joinedByCommas())
+            case .betaAppReviewDetails(let value):
+                return (BetaAppReviewDetail.key, value.map({ $0.pair.value }).joinedByCommas())
+            }
+        }
+    }
+    
+    /// Attributes, relationships, and IDs by which to filter.
+    public enum Filter: NestableQueryParameter {
+        case app([String])
+
+        static var key: String = "filter"
+        var pair: Pair {
+            switch self {
+            case .app(let value):
+                return ("app", value.joinedByCommas())
+            }
+        }
+    }
+    
+    /// Relationship data to include in the response.
+    public enum Include: String, CaseIterable, NestableQueryParameter {
+        case app
+
+        static var key: String = "include"
+        var pair: NestableQueryParameter.Pair { return (nil, rawValue) }
+    }
+}
+
+extension ListBetaAppReviewDetails.Field {
+    
+    public enum App: String, CaseIterable, NestableQueryParameter {
+        case betaAppLocalizations, betaAppReviewDetail, betaGroups, betaLicenseAgreement, betaTesters, builds, bundleId, name, preReleaseVersions, primaryLocale, sku
+
+        static var key: String = "app"
+        var pair: NestableQueryParameter.Pair { return (nil, rawValue) }
+    }
+    
+    public enum BetaAppReviewDetail: String, CaseIterable, NestableQueryParameter {
+        case app, contactEmail, contactFirstName, contactLastName, contactPhone, demoAccountName, demoAccountPassword, demoAccountRequired, notes
+
+        static var key: String = "betaAppReviewDetail"
+        var pair: NestableQueryParameter.Pair { return (nil, rawValue) }
+    }
+}
