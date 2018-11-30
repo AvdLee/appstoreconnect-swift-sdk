717aed3bd24187478cb73988d50d185ca1df56aa
Pascal Edmond <pe@pierlis.com>
2018-11-30 10:39:56 +0100

    CreateBetaTester: 409  Only one relationship should be included when creating betaTesters

--- Sources/Endpoints/TestFlight/BetaTesters/CreateBetaTester.swift
+++ Sources/Endpoints/TestFlight/BetaTesters/CreateBetaTester.swift
@@ -17,20 +17,45 @@ extension APIEndpoint where T == BetaTesterResponse {
     ///   - firstName: The beta tester's first name.
     ///   - lastName: The beta tester's last name.
     ///   - betaGroupIds: Array of opaque resource ID that uniquely identifies the resources.
-    ///   - buildIds: Array of opaque resource ID that uniquely identifies the resources.
     ///
-    /// - Attention: Either non empty betaGroupIds or non empty buildIds is required when creating betaTesters.
+    /// - Attention: Non empty betaGroupIds is required when creating betaTesters.
     public static func create(
         betaTesterWithEmail email: String,
         firstName: String? = nil,
         lastName: String? = nil,
-        betaGroupIds: [String]? = nil,
-        buildIds: [String]? = nil) -> APIEndpoint {
+        betaGroupIds: [String]) -> APIEndpoint {
         let betaTester = BetaTesterCreateRequest(
             email: email,
             firstName: firstName,
             lastName: lastName,
             betaGroupIds: betaGroupIds,
+            buildIds: nil)
+        return APIEndpoint(
+            path: "betaTesters",
+            method: .post,
+            parameters: nil,
+            body: try? JSONEncoder().encode(betaTester))
+    }
+    
+    /// Create a beta tester assigned to a group, a build, or an app.
+    ///
+    /// - Parameters:
+    ///   - email: The beta tester's email address, used for sending beta testing invitations.
+    ///   - firstName: The beta tester's first name.
+    ///   - lastName: The beta tester's last name.
+    ///   - buildIds: Array of opaque resource ID that uniquely identifies the resources.
+    ///
+    /// - Attention: Non empty buildIds is required when creating betaTesters.
+    public static func create(
+        betaTesterWithEmail email: String,
+        firstName: String? = nil,
+        lastName: String? = nil,
+        buildIds: [String]) -> APIEndpoint {
+        let betaTester = BetaTesterCreateRequest(
+            email: email,
+            firstName: firstName,
+            lastName: lastName,
+            betaGroupIds: nil,
             buildIds: buildIds)
         return APIEndpoint(
             path: "betaTesters",
