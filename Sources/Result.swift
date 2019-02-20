bf41b7c76e9ef3ac678ce41b5304516284d1ba9a
Michael Schwarz <michael_schwarz@me.com>
2019-02-20 08:56:57 +0100

    Remove excessive whitespace

--- Sources/Result.swift
+++ Sources/Result.swift
@@ -21,7 +21,7 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 //
-//  swiftlint:disable nslog_prohibited
+// swiftlint:disable nslog_prohibited
 
 import Foundation
 
