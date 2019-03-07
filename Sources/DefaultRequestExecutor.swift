473c276b88e33004c9dbc48f4f36425af613e2c9
Michael Schwarz <michael_schwarz@me.com>
2019-03-07 20:59:32 +0100

    Add more comments

--- Sources/DefaultRequestExecutor.swift
+++ Sources/DefaultRequestExecutor.swift
@@ -11,6 +11,7 @@ import Foundation
 public final class DefaultRequestExecutor: RequestExecutor {
 
     enum Error: Swift.Error {
+        // if the response is no HTTPURLResponse
         case unknownResponseType
     }
 
@@ -20,12 +21,22 @@ public final class DefaultRequestExecutor: RequestExecutor {
 
     // MARK: - RequestExecutor
 
+    /// Executes an URLRequest and delivers an async result
+    ///
+    /// - Parameters:
+    ///   - urlRequest: The URLRequest to execute
+    ///   - completion: A result type containing eiter the response or an error
     public func execute(_ urlRequest: URLRequest, completion: @escaping (Result<Response>) -> Void) {
         urlSession.dataTask(with: urlRequest) { data, response, error in
             completion(mapResponse(data: data, urlResponse: response, error: error))
         }.resume()
     }
 
+    /// Retrieves a resource and delivers an async result
+    ///
+    /// - Parameters:
+    ///   - url: The URL where the resource is located
+    ///   - completion: A result type containing eiter the response or an error
     public func retrieve(_ url: URL, completion: @escaping (Result<Response>) -> Void) {
         urlSession.dataTask(with: url) { data, response, error in
             completion(mapResponse(data: data, urlResponse: response, error: error))
@@ -35,6 +46,13 @@ public final class DefaultRequestExecutor: RequestExecutor {
 
 // MARK: - Private
 
+/// Maps the result of an URLSession dataTask to a response type
+///
+/// - Parameters:
+///   - data: Data returned from an URLSession data task
+///   - urlResponse: URLResponse returned from an URLSession data task
+///   - error: Error returned from an URLSession data task
+///   - completion: A result type containing eiter the response or an error
 func mapResponse(data: Data?, urlResponse: URLResponse?, error: Error?) -> Result<Response> {
     if let error = error {
         return .failure(error)
