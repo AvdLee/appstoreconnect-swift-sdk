94a9af697aeffe5d66b48bd0bcb0cb3589436dff
Rui Costa <rui.pfcosta@gmail.com>
2019-12-07 11:31:21 +0000

    Tweak styling

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     name: "AppStoreConnect-Swift-SDK",
     platforms: [
         .iOS(.v11),
-        .macOS(.v10_12),
+        .macOS(.v10_12)
     ],
     products: [
         .library(
