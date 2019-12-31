cf1b7be28cc47c0184820d1ee416e033130e0dd9
Rui Costa <rui.pfcosta@gmail.com>
2019-12-31 10:38:44 +0000

    Minor tweaks

--- Sources/Models/BundleIdUpdateRequest.swift
+++ Sources/Models/BundleIdUpdateRequest.swift
@@ -5,20 +5,19 @@
 //  Created by Rui Costa on 30/12/2019.
 //
 
-
 import Foundation
 
 /// A request containing a single resource.
 public struct BundleIdUpdateRequest: Encodable {
-    
-    /// The resource data.
-    public let data: Data
     
     public struct Data: Encodable {
         public let id: String
         public let attributes: Attributes
         public let type: String = "bundleIds"
     }
+    
+    /// The resource data.
+    public let data: Data
 }
 
 // MARK: BundleIdUpdateRequest.Data
@@ -28,4 +27,3 @@ extension BundleIdUpdateRequest.Data {
         public let name: String
     }
 }
-
