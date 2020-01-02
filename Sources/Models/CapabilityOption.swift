8720d13342e3edc54456d707a7b71e622a1ee284
BalestraPatrick <me@patrickbalestra.com>
2020-01-02 23:25:03 +0100

    Remove useless newline character between properties

--- Sources/Models/CapabilityOption.swift
+++ Sources/Models/CapabilityOption.swift
@@ -9,16 +9,10 @@ import Foundation
 
 /// The data structure that represents the resource.
 public struct CapabilityOption: Codable {
-
     public let description: String?
-
     public let enabled: Bool?
-
     public let enabledByDefault: Bool?
-
     public let key: CapabilityOptionKey?
-
     public let name: String?
-
     public let supportsWildcard: Bool?
 }
--- Sources/Models/CapabilitySetting.swift
+++ Sources/Models/CapabilitySetting.swift
@@ -9,20 +9,12 @@ import Foundation
 
 /// The data structure that represents the resource.
 public struct CapabilitySetting: Codable {
-
     public let allowedInstances: CapabilitySettingAllowedInstances?
-
     public let description: String?
-
     public let enabledByDefault: Bool?
-
     public let key: CapabilitySettingKey?
-
     public let name: String?
-
     public let options: [CapabilityOption]?
-
     public let visible: Bool?
-
     public let minInstances: Int?
 }
