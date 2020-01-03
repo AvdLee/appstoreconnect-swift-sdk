294f19cc734120b7b2eada9b20f81774c3deb703
BalestraPatrick <me@patrickbalestra.com>
2020-01-03 12:39:57 +0100

    Simplify BundleIdCapabilityResponse model

--- Sources/Models/BundleIdCapabilityResponse.swift
+++ Sources/Models/BundleIdCapabilityResponse.swift
@@ -7,69 +7,13 @@
 
 import Foundation
 
+
 /// The data structure that represents the resource.
 public struct BundleIdCapabilityResponse: Codable {
 
-    /// Attributes that describe a resource.
-    public struct Attributes: Codable {
-        
-        /// The type of the capabillity.
-        public let capabilityType: CapabilityType?
-        
-        /// Specific settings for this capability.
-        public let settings: [CapabilitySetting]?
-    }
-    
-    public struct Relationships: Codable {
-        
-        /// BundleIdCapabilityResponse.Relationships.BundleId
-        public let bundleId: BundleIdCapabilityResponse.Relationships.BundleId?
-    }
-
-    /// The resource's attributes.
-    public let attributes: BundleIdCapabilityResponse.Attributes?
-
-    /// Navigational links to related data and included resource types and IDs.
-    public let relationships: BundleIdCapabilityResponse.Relationships?
-
-    /// The resource type.Value: bundleIdCapabilities
-    public let type: String = "bundleIdCapabilities"
+    /// The resource data.
+    public let data: BundleIdCapability?
 
     /// Navigational links that include the self-link.
-    public let links: ResourceLinks<BundleIdCapabilityResponse>
-}
-
-// MARK: BundleIdCapabilityResponse.Relationships
-extension BundleIdCapabilityResponse.Relationships {
-
-    public struct BundleId: Codable {
-
-        /// BundleIdCapabilityResponse.Relationships.BundleId.Data
-        public let data: BundleIdCapabilityResponse.Relationships.BundleId.Data?
-
-        /// BundleIdCapabilityResponse.Relationships.BundleId.Links
-        public let links: BundleIdCapabilityResponse.Relationships.BundleId.Links?
-    }
-}
-
-// MARK: BundleIdCapabilityResponse.Relationships.BundleId
-extension BundleIdCapabilityResponse.Relationships.BundleId {
-
-    public struct Data: Codable {
-
-        /// The opaque resource ID that uniquely identifies the resource.
-        public let `id`: String
-
-        /// The resource type.Value: bundleIds
-        public let type: String = "bundleIds"
-    }
-
-    public struct Links: Codable {
-
-        /// uri-reference
-        public let related: URL?
-
-        /// uri-reference
-        public let `self`: URL?
-    }
+    public let links: DocumentLinks
 }
