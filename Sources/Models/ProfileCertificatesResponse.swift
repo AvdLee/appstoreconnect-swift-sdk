873e87f3ed6cd9616e933bf3d3d98202b7741988
Kristof Van Landschoot <211518+ristkof@users.noreply.github.com>
2020-03-19 20:11:05 +0100

    Wrong response type for getAllCertificates call (#72)
    * Wrong response type for this call
    
    This call has a response that corresponds to ProfileDevicesLinkagesResponse, not ProfileResponse. According to the documentation it should be a CertificatesResponse (see https://developer.apple.com/documentation/appstoreconnectapi/list_all_certificates_in_a_profile), but that's not correct either, since the attributes are missing from the certificates in the data array.
    
    An alternative option could be to make the attributes optional in certificate and use CertificatesResponse here.
    
    For now I propose to abuse ProfileDevicesLinkagesResponse or else create a new response type specifically for this.
    
    What do you think?
    
    * Create a new response type that matches the outcome
    
    * Add ProfilesCertificatesResponse file to project
    
    * Update Sources/Models/ProfileCertificatesResponse.swift
    
    Co-authored-by: Antoine van der Lee <ajvanderlee@gmail.com>
    Co-authored-by: Kristof Van Landschoot <kristofgit@ristkof.be>
    Co-authored-by: Antoine van der Lee <antoine@wetransfer.com>

---
+++ Sources/Models/ProfileCertificatesResponse.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileCertificatesResponse.swift
+//  AppStoreConnect-Swift-SDK
+//
+//  Created by Kristof Van Landschoot on 03/14/20.
+//
+
+import Foundation
+
+/// A response containing a list of related resource IDs.
+public struct ProfileCertificatesResponse: Codable {
+
+    public struct Data: Codable {
+
+        /// The opaque resource ID that uniquely identifies the resource.
+        public let `id`: String
+
+        /// The resource type.Value: devices
+        public let type: String = "certificates"
+    }
+
+    /// The object types and IDs of the related resources.
+    public let data: [ProfileCertificatesResponse.Data]
+
+    /// Navigational links including the self-link and links to the related data.
+    public let links: DocumentLinks
+
+    /// Paging information.
+    public let meta: PagingInformation?
+}
