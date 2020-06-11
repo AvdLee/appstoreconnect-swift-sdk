00f3f333e3f3c6a5c5767dee944401dd8eaa72ce
Decheng <30752103+DechengMa@users.noreply.github.com>
2020-06-11 19:20:38 +1000

    Add public init to CapabilitySetting and CapabilityOption (#106)

--- Sources/Models/CapabilityOption.swift
+++ Sources/Models/CapabilityOption.swift
@@ -15,4 +15,20 @@ public struct CapabilityOption: Codable {
     public let key: CapabilityOptionKey?
     public let name: String?
     public let supportsWildcard: Bool?
+
+    public init(
+        description: String? = nil,
+        enabled: Bool? = nil,
+        enabledByDefault: Bool? = nil,
+        key: CapabilityOptionKey? = nil,
+        name: String? = nil,
+        supportsWildcard: Bool? = nil
+    ) {
+        self.description = description
+        self.enabled = enabled
+        self.enabledByDefault = enabledByDefault
+        self.key = key
+        self.name = name
+        self.supportsWildcard = supportsWildcard
+    }
 }
