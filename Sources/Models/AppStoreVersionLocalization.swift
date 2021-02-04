f50bd4e96b80c216abc22f02c330523a9bbff402
Kirill Budevich <Ikloo@yandex.ru>
2021-02-04 12:46:27 +0300

    Feature/app store version localization (#130)
    * Add support for AppStoreVersionLocalizations
    
    Added `ListAppStoreVersionLocalizations` endpoint
    Added `AppStoreVersionLocalization`, `AppStoreVersionLocalizationRelationship`, `AppStoreVersionLocalizationResponse`, `AppStoreVersionLocalizationsResponse` models
    
    * Restore missing project groups
    
    * Add AppStoreVersionLocalizations Tests
    
    * Remove commented relationship
    
    * Add AppStoreVersion
    
    Added currently available AppStoreVersion to AppStoreVersionLocalizationRelationship

---
+++ Sources/Models/AppStoreVersionLocalization.swift
@@ -0,0 +1,91 @@
+//
+//  AppStoreVersionLocalization.swift
+//
+//  Created by Kirill Budevich on 1/27/21.
+//
+
+import Foundation
+
+/// The data structure that represents the resource.
+public struct AppStoreVersionLocalization: Codable {
+    /// Attributes that describe a resource.
+    public struct Attributes: Codable {
+        public let description: String?
+        public let keywords: String?
+        public let locale: String?
+        public let marketingUrl: URL?
+        public let promotionalText: String?
+        public let supportUrl: URL?
+        public let whatsNew: String?
+    }
+    
+    public struct Relationships: Codable {
+        public let appPreviewSets: AppStoreVersionLocalization.Relationships.AppPreviewSets?
+        public let appScreenshotSets: AppStoreVersionLocalization.Relationships.AppScreenshotSets?
+        public let appStoreVersion: AppStoreVersionLocalization.Relationships.AppStoreVersion?
+    }
+    
+    /// The resource's attributes.
+    public let attributes: AppStoreVersionLocalization.Attributes?
+    
+    /// The opaque resource ID that uniquely identifies the resource.
+    public let id: String
+    
+    // Navigational links that include the self-link.
+    public let links: ResourceLinks<AppStoreVersionLocalizationResponse>
+
+    /// Navigational links to related data and included resource types and IDs.
+    public let relationships: AppStoreVersionLocalization.Relationships?
+    
+    /// The resource type.Value: appStoreVersionLocalizations
+    public let type: String = "appStoreVersionLocalizations"
+    
+}
+
+// MARK: AppStoreVersionLocalization.Relationships
+extension AppStoreVersionLocalization.Relationships {
+    public struct AppPreviewSets: Codable {
+        public struct Data: Codable {
+            public let `id`: String
+            public let type: String = "appPreviewSets"
+        }
+        
+        public struct Links: Codable {
+            public let related: URL?
+            public let `self`: URL?
+        }
+        
+        public let data: Data?
+        public let links: Links?
+    }
+    
+    public struct AppScreenshotSets: Codable {
+        public struct Data: Codable {
+            public let `id`: String
+            public let type: String = "appScreenshotSets"
+        }
+        
+        public struct Links: Codable {
+            public let related: URL?
+            public let `self`: URL?
+        }
+        
+        public let data: Data?
+        public let links: Links
+    }
+    
+    public struct AppStoreVersion: Codable {
+        public struct Data: Codable {
+            public let `id`: String
+            public let type: String = "appStoreReviewDetails"
+        }
+        
+        public struct Links: Codable {
+            public let related: URL?
+            public let `self`: URL?
+        }
+        
+        public let data: Data?
+        public let links: Links?
+    }
+}
---
+++ Sources/Models/AppStoreVersionLocalizationRelationship.swift
@@ -0,0 +1,32 @@
+//
+//  AppStoreVersionLocalizationRelationship.swift
+//
+//  Created by Kirill Budevich on 1/27/21.
+//
+
+import Foundation
+
+public enum AppStoreVersionLocalizationRelationship: Codable {
+    case appStoreVersion(AppStoreVersion)
+    
+    enum TypeKeys: String, CodingKey {
+        case type
+    }
+    enum CodingKeys: String, Decodable, CodingKey {
+        case appStoreVersion
+    }
+
+    public init(from decoder: Decoder) throws {
+        switch try decoder.container(keyedBy: TypeKeys.self).decode(CodingKeys.self, forKey: .type) {
+        case .appStoreVersion:
+            self = try .appStoreVersion(AppStoreVersion(from: decoder))
+        }
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        switch self {
+        case .appStoreVersion(let value):
+            try value.encode(to: encoder)
+        }
+    }
+}
