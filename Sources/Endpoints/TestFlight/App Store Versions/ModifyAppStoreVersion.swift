8d01a5f3c7d5a7a707f56c90cebbc66f61b61f7c
Scott Lemke <scott@ibotta.com>
2021-05-13 23:06:28 -0600

    Models and endpoint for Modify an App Store Version

---
+++ Sources/Endpoints/TestFlight/App Store Versions/ModifyAppStoreVersion.swift
@@ -0,0 +1,31 @@
+import Foundation
+
+public extension APIEndpoint where T == AppStoreVersionResponse {
+    static func modify(
+        appStoreVersionWithId id: String,
+        buildId: String? = nil,
+        copyright: String? = nil,
+        earliestReleaseDate: Date? = nil,
+        releaseType: AppStoreVersionUpdateRequest.Data.Attributes.ReleaseType? = nil,
+        usesIdfa: Bool? = nil,
+        versionString: String? = nil,
+        downloadable: Bool? = nil
+    ) -> APIEndpoint {
+        let request = AppStoreVersionUpdateRequest(
+            id: id,
+            buildId: buildId,
+            copyright: copyright,
+            earliestReleaseDate: earliestReleaseDate,
+            releaseType: releaseType,
+            usesIdfa: usesIdfa,
+            versionString: versionString,
+            downloadable: downloadable
+        )
+        return APIEndpoint(
+            path: "appStoreVersions/\(id)",
+            method: .patch,
+            parameters: nil,
+            body: try? JSONEncoder().encode(request)
+        )
+    }
+}
