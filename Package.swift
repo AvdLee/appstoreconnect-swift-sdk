4b3a8a51c81f324eb834e33a783252b7e3cebc18
Antoine van der Lee <ajvanderlee@gmail.com>
2021-11-22 19:23:36 +0100

    Update package.swift

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.1
+// swift-tools-version:5.5
 // We're hiding dev, test, and danger dependencies with // dev to make sure they're not fetched by users of this package.
 
 import PackageDescription
@@ -14,11 +14,14 @@ let package = Package(
         .library(name: "AppStoreConnect-Swift-SDK", targets: ["AppStoreConnect-Swift-SDK"])
     ],
     dependencies: [
-        // dev .package(url: "https://github.com/danger/swift", from: "3.0.0"),
-        // dev .package(path: "Submodules/WeTransfer-iOS-CI/Danger-Swift")
+        // dev .package(url: "https://github.com/danger/swift", from: "3.12.1"),
+        // dev .package(name: "WeTransferPRLinter", path: "Submodules/WeTransfer-iOS-CI/WeTransferPRLinter")
         ],
     targets: [
-        // dev .target(name: "DangerDependencies", dependencies: ["Danger", "WeTransferPRLinter"], path: "Submodules/WeTransfer-iOS-CI/Danger-Swift", sources: ["DangerFakeSource.swift"]),
+        // dev .target(name: "DangerDependencies", dependencies: [
+        // dev     .product(name: "Danger", package: "danger-swift"),
+        // dev     .product(name: "WeTransferPRLinter", package: "WeTransferPRLinter")
+        // dev ], path: "Submodules/WeTransfer-iOS-CI/DangerFakeSources", sources: ["DangerFakeSource.swift"])
         // dev .testTarget(name: "AppStoreConnect-Swift-SDK-Tests", dependencies: ["AppStoreConnect-Swift-SDK"], path: "Tests", exclude: ["LinuxMain.swift"]),
         .target(name: "AppStoreConnect-Swift-SDK", path: "Sources")
     ]
