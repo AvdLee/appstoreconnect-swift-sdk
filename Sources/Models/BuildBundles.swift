11ef7025ef651b2bcd73c44b94e52aa6dc96f3f0
Bartosz Cichecki <cichecki.bartosz@gmail.com>
2021-12-21 14:05:24 +0100

    Added includesSymbols

--- Sources/Models/BuildBundles.swift
+++ Sources/Models/BuildBundles.swift
@@ -19,6 +19,9 @@ public struct BuildBundles: Codable {
         // The type of the build bundle.
         public let bundleType: String
 		
+		// True if build bundle contains symbols
+		public let includesSymbols: Bool
+		
 		// URL to the dSYM if available
 		public let dSYMUrl: URL?
     }
