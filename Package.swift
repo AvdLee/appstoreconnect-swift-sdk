a4ebc9fb6b8c6f5861f2dd3b3d318b1bfb68add1
Orestis Papadopoulos <orjpap@gmail.com>
2022-02-14 23:23:17 +0200

    Moved iOS minimum deployment target to iOS 13.0

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "AppStoreConnect-Swift-SDK",
     platforms: [
-        .iOS(.v11),
+        .iOS(.v13),
         .macOS(.v10_15)
     ],
     products: [
