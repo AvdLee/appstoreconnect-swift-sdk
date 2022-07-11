a354131174d05f6ba24a30e520a29904ba92b87b
Antoine van der Lee <ajvanderlee@gmail.com>
2022-07-11 19:12:30 +0200

    Started with new SwiftUI example

---
+++ Example/AppStoreConnectAPIExample/Shared/AppStoreConnectAPIExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreConnectAPIExampleApp.swift
+//  Shared
+//
+//  Created by Antoine van der Lee on 11/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreConnectAPIExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
