b25a7599c3c969ef7cd4e800f0c3997a0f49f7f6
Antoine van der Lee <ajvanderlee@gmail.com>
2022-07-11 19:19:39 +0200

    Update example project using SwiftUI

--- Example/Shared/AppStoreConnectAPIExampleApp.swift
+++ Example/Shared/AppStoreConnectAPIExampleApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct AppStoreConnectAPIExampleApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AppsListView()
         }
     }
 }
