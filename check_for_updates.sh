df2d9789a62309693c68051ac488d0bc8686071b
Pol Piella Abadia <hi@polpiella.dev>
2024-11-13 08:29:04 +0100

    Creating a PR should now work

--- check_for_updates.sh
+++ check_for_updates.sh
@@ -2,10 +2,11 @@
 
 echo "Checking for updates..."
 
+CURRENT_VERSION=$(cat Sources/OpenAPI/app_store_connect_api.json | jq -r '.info.version')
+
 make download
 
 NEW_VERSION=$(cat Sources/OpenAPI/app_store_connect_api.json | jq -r '.info.version')
-CURRENT_VERSION=$(cat spec-version)
 REMOTE_BRANCH=$(git ls-remote origin refs/heads/spec-update-$NEW_VERSION)
 
 echo "Current version: $CURRENT_VERSION"
@@ -15,8 +16,6 @@ if [ "$NEW_VERSION" == "$CURRENT_VERSION" ] || [ ! -z $REMOTE_BRANCH ] ; then
     echo "Spec version is up to date"
     exit 0
 fi
-
-echo $NEW_VERSION > spec-version
 
 make generate
 
